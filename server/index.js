20e0784be4b0ba1e08177dc476dd1bad0223eeec
Sergey Sherstobitov <in43sh@gmail.com>
2017-12-22 11:50:56 -0700

    Added user login/registration. For now just with a object in file.

--- server/index.js
+++ server/index.js
@@ -4,18 +4,27 @@ const session = require('express-session');
 
 require('dotenv').config();
 
-const app = express();
-app.use( bodyParser.json() );
+// Middleware
+const checkForSession = require('./middlewares/checkForSession');
 
+// Controllers
+const auth_controller = require('./controllers/auth_controller');
+
+const app = express();
+
+app.use( bodyParser.json() );
 app.use( session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
+app.use( checkForSession );
 
-app.get('/api/test', (req, res)=> {
-  res.status(200).send('http://i63.tinypic.com/2j9y8p.png');
-})
+// Authorisation
+app.post( '/api/auth/login', auth_controller.login );
+app.post( '/api/auth/register', auth_controller.register );
+app.post( '/api/auth/logout', auth_controller.logout );
+app.get( '/api/auth/user', auth_controller.getUser );
 
 const port = process.env.PORT || 3333;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
